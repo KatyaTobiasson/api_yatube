b1ab115c19941008a4d87d8b08d42e0f7453ecc0
“Ekaterina <Katjatobiasson@gmail.comgit config --global user.name Ekaterina Stepanchukgit config --global user.email Katjatobiasson@gmail.com>
2023-02-04 00:18:25 +0800

    f8 3rd

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -88,15 +88,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-        ],
-
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
+    'rest_framework.permissions.IsAuthenticated', ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        ],
-                 }
+    'rest_framework.authentication.TokenAuthentication', ],
+}
 
 SIMPLE_JWT = {
     # Устанавливаем срок жизни токена
